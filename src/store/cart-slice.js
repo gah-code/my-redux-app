70d8dbe51647a3d7dd8b5af5a16c99bd226858a9
Gilbert Haro <gilbertharocode@gmail.com>
2022-12-20 17:48:29 -0800

    fetching data with useEffect

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
